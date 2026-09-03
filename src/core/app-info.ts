a5cecd4045252329a5a3015aefff5045312df5ea
lam <wenwenwen888@live.com>
2026-09-03 17:42:12 +0800

    Release v0.1.10

--- src/core/app-info.ts
+++ src/core/app-info.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "Image Slicing Tools";
 export const APP_NAME_ZH = "图片切图工具";
-export const APP_VERSION = "0.1.9";
+export const APP_VERSION = "0.1.10";
 export const APP_AUTHOR = "Lam Wan";
