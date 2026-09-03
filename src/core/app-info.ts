3dd0453205620832793bb1993cd07346684be788
lam <wenwenwen888@live.com>
2026-09-03 16:37:46 +0800

    Release v0.1.8

--- src/core/app-info.ts
+++ src/core/app-info.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "Image Slicing Tools";
 export const APP_NAME_ZH = "图片切图工具";
-export const APP_VERSION = "0.1.7";
+export const APP_VERSION = "0.1.8";
 export const APP_AUTHOR = "Lam Wan";
